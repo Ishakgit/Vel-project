8c7b7f4f2e9bb565ea91e05d16dcc80ff15babf6
Ishak <mdishakeee@gmail.com>
2022-03-14 10:52:27 +0530

    new file

---
+++ src/org/task/Git.java
@@ -0,0 +1,26 @@
+package org.task;
+
+public class Git {
+	
+	
+	public static void main(String[] args) {
+		
+		int a=10,b=20; 
+		
+		b=a;
+		a=b;
+		
+		System.out.println(a);
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
